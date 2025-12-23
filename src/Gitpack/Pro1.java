385768229e7f55788c55f3370bc19bf7dfcc7c33
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-23 10:42:47 +0530

    mess

---
+++ src/Gitpack/Pro1.java
@@ -0,0 +1,26 @@
+package Gitpack;
+
+import java.util.Scanner;
+
+
+public class Pro1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();        // size of array
+        int[] ar = new int[n];       // array creation
+
+        // taking input
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        // printing array elements
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(ar[i] + " ");
+        }
+    }
+
+
+}
